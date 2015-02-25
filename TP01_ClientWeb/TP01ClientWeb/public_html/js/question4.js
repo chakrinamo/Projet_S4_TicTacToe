4d228eb6b8dd0eab3fbb7f5b90144905a42aa60c
chakrinamo <mouhouni.chakrina@iut-dijon.u-bourgogne.fr>
2015-02-25 19:49:43 +0100

    pt

--- TP01_ClientWeb/TP01ClientWeb/public_html/js/question4.js
+++
@@ -1,8 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function comparerLesFonctions(){
-    console.log(personne1.getNom === personne2.getNom);
-}
